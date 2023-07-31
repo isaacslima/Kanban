712a502d6999a20b5e22eedb6c088fa9a9b79b9a
Isaac de Sousa <isaacslima@gmail.com>
2023-07-31 14:30:32 -0300

    add entity card and dependency Application project

--- BACK/Core/Domain/Kanban.Domain/Card.cs
+++ BACK/Core/Domain/Kanban.Domain/Card.cs
@@ -1,7 +1,12 @@
-﻿namespace Kanban.Domain
-{
-    public class Card
-    {
+﻿namespace Kanban.Domain;
 
-    }
+public class Card
+{
+    public Guid Id { get; set; }
+
+    public string Titulo { get; set; } = null!;
+
+    public string Conteudo { get; set; } = null!;
+
+    public string Lista { get; set; } = null!;
 }
